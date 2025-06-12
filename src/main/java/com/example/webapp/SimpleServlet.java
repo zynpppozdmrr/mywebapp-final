32bfa320d5f0ebc2b9d2acc2dcfb3dff3f1c5e4b
DESKTOP-F4UA4G4\zeynepozdemir <ozdemirzeynep097@gmail.com>
2025-06-13 00:17:16 +0300

    /hello endpoint updated

--- src/main/java/com/example/webapp/SimpleServlet.java
+++ src/main/java/com/example/webapp/SimpleServlet.java
@@ -1,42 +1,33 @@
 package com.example.webapp;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 @WebServlet("/hello")
 public class SimpleServlet extends HttpServlet {
-
-    private Logger log;
-
-    @Override
-    public void init() throws ServletException {
-        System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$-7s] %5$s %n");
-        this.log = Logger.getLogger(SimpleServlet.class.getName());
-        super.init();
-    }
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        log.log(Level.INFO, "GET Request received.");
-
+        // Azure App Service environment variables
         String dbHost = System.getenv("POSTGRES_HOST");
         String dbUser = System.getenv("POSTGRES_USER");
-        String dbPass = System.getenv("POSTGRES_PASSWORD");
+        
         String dbName = System.getenv("POSTGRES_DATABASE");
 
-        resp.setStatus(HttpServletResponse.SC_OK);
         resp.setContentType("text/plain");
-        resp.getWriter().println("Database Host: " + dbHost);
-        resp.getWriter().println("Database User: " + dbUser);
-        resp.getWriter().println("Database Name: " + dbName);
+        PrintWriter out = resp.getWriter();
+
+        out.println("Database Host: " + dbHost);
+        out.println("Database User: " + dbUser);
+        out.println("Database Name: " + dbName);
+        // Güvenlik açısından şifreyi yazdırmıyoruz
+        // out.println("Database Password: " + dbPassword);
     }
 }
